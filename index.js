eb9bda09d3b8ade49650cb7b462c26b6194048a0
rohan-nordstone <rohan@nordstone.co.uk>
2024-04-02 20:55:14 +0530

    Cors fixed

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const express = require('express');
 const connectDB = require('./config/db');
 const productRoutes = require('./routes/products');
 const orderRoutes = require('./routes/orders')
+const cors = require('cors');
 const app = express();
 connectDB();
 
 // Middleware
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
