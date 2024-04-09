11d418a5b8886b4274859e8714c29789dabaccbc
rohan-nordstone <rohan@nordstone.co.uk>
2024-04-09 16:45:19 +0530

    timestamps added in order api

--- models/order.js
+++ models/order.js
@@ -1,29 +1,34 @@
 const mongoose = require('mongoose');
 
-const OrderSchema = new mongoose.Schema({
+const OrderSchema = new mongoose.Schema(
+  {
     user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
     },
     shippingDetails: {
-        name: String,
-        mobileNumber: String,
-        houseStreet: String,
-        cityTown: String,
-        state: String,
-        pincode: String
+      name: String,
+      mobileNumber: String,
+      houseStreet: String,
+      cityTown: String,
+      state: String,
+      pincode: String,
     },
     shippingMethod: String,
     paymentMethod: String,
-    cartItems: [{
+    cartItems: [
+      {
         id: String,
         name: String,
         price: Number,
         quantity: Number,
-        imageUrl: [String]
-    }],
-    cartTotal: Number
-});
+        imageUrl: [String],
+      },
+    ],
+    cartTotal: Number,
+  },
+  { timestamps: true } // This option adds createdAt and updatedAt fields automatically
+);
 
 const Order = mongoose.model('Order', OrderSchema);
 
